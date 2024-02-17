cfb5e097925ca181450ad9badc8d0adcd1b56b8c
hussein-awala <hussein@awala.fr>
2024-02-17 19:46:16 +0100

    add super class arguments to AsyncDynamoDbGetBatcher

--- async_batcher/aws/dynamodb/get.py
+++ async_batcher/aws/dynamodb/get.py
@@ -20,6 +20,25 @@ class GetItem:
 
 
 class AsyncDynamoDbGetBatcher(AsyncBatcher[GetItem, dict[str, Any]]):
+    """Batcher for DynamoDB GetItem operation. It uses aioboto3 to interact with DynamoDB.
+
+    Args:
+        region_name: The region to use.
+        use_ssl: Whether to use SSL/TLS.
+        verify: Whether to verify SSL certificates.
+        endpoint_url: The complete URL to use for the constructed client. This is useful for local testing.
+        config: The configuration for the session.
+        aioboto3_session: The aioboto3 session to use. If not provided, a new session is created.
+        batch_size: The maximum number of items to process in a single batch. The default is 100 items,
+            which is the maximum number of items that can be processed in a single batch.
+        sleep_time (float, optional): The time to sleep between checking if the result is ready in seconds.
+            Defaults to 0.01. Set it to a value close to the expected time to process a batch
+        buffering_time (float, optional): The time to sleep after processing a batch or checking the buffer
+            in seconds. Defaults to 0.001.
+            You can increase this value if you don't need a low latency, but want to reduce the number of
+            processed batches.
+    """
+
     def __init__(
         self,
         *,
@@ -29,9 +48,11 @@ class AsyncDynamoDbGetBatcher(AsyncBatcher[GetItem, dict[str, Any]]):
         endpoint_url: str | None = None,
         config: AioConfig | None = None,
         aioboto3_session: aioboto3.Session | None = None,
-        **kwargs,
+        batch_size: int = 100,
+        sleep_time: float = 0.01,
+        buffering_time: float = 0.001,
     ):
-        super().__init__(**kwargs)
+        super().__init__(batch_size=batch_size, sleep_time=sleep_time, buffering_time=buffering_time)
         self.region_name = region_name
         self.use_ssl = use_ssl
         self.verify = verify
