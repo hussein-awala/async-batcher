e4b68d0e9bf6fedbccfc30316753ccc07cefeb19
hussein-awala <hussein@awala.fr>
2024-02-18 01:34:10 +0100

    implemenet an async batcher for sklearn models

---
+++ async_batcher/ml/sklearn/model.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from async_batcher.batcher import AsyncBatcher
+
+if TYPE_CHECKING:
+    from sklearn.base import BaseEstimator
+
+
+class SklearnAsyncBatcher(AsyncBatcher):
+    def __init__(
+        self,
+        *,
+        model: BaseEstimator,
+        batch_size: int = -1,
+        sleep_time: float = 0.01,
+        buffering_time: float = 0.001,
+    ):
+        super().__init__(batch_size=batch_size, sleep_time=sleep_time, buffering_time=buffering_time)
+        self.model = model
+
+    def process_batch(self, batch):
+        if hasattr(self.model, "predict"):
+            return self.model.predict(batch)
+        else:
+            raise AttributeError("Model does not have a predict method")
