46a637843e8b5204c1703363989279eaf004cc7b
hussein-awala <hussein@awala.fr>
2024-02-18 18:24:12 +0100

    support executing ml predict in a custom executor

--- async_batcher/ml/keras/model.py
+++ async_batcher/ml/keras/model.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+import asyncio
 from typing import TYPE_CHECKING
 
 from async_batcher.batcher import AsyncBatcher
 
 if TYPE_CHECKING:
+    from concurrent.futures import Executor
+
     from keras import Model
 
 
@@ -13,12 +16,17 @@ class SklearnAsyncBatcher(AsyncBatcher):
         self,
         *,
         model: Model,
+        executor: Executor | None = None,
         batch_size: int = -1,
         sleep_time: float = 0.01,
         buffering_time: float = 0.001,
     ):
         super().__init__(batch_size=batch_size, sleep_time=sleep_time, buffering_time=buffering_time)
         self.model = model
+        self.executor = executor
 
     async def process_batch(self, batch):
-        return self.model.predict(batch, batch_size=len(batch))
+        result = await asyncio.get_event_loop().run_in_executor(
+            self.executor, lambda _batch: self.model.predict(_batch, batch_size=len(_batch)), batch
+        )
+        return result
--- async_batcher/ml/sklearn/model.py
+++ async_batcher/ml/sklearn/model.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
+import asyncio
 from typing import TYPE_CHECKING
 
 from async_batcher.batcher import AsyncBatcher
 
 if TYPE_CHECKING:
+    from concurrent.futures import Executor
+
     from sklearn.base import BaseEstimator
 
 
@@ -13,15 +16,18 @@ class SklearnAsyncBatcher(AsyncBatcher):
         self,
         *,
         model: BaseEstimator,
+        executor: Executor | None = None,
         batch_size: int = -1,
         sleep_time: float = 0.01,
         buffering_time: float = 0.001,
     ):
         super().__init__(batch_size=batch_size, sleep_time=sleep_time, buffering_time=buffering_time)
         self.model = model
+        self.executor = executor
 
     async def process_batch(self, batch):
         if hasattr(self.model, "predict"):
-            return self.model.predict(batch)
+            result = await asyncio.get_event_loop().run_in_executor(self.executor, self.model.predict, batch)
+            return result
         else:
             raise AttributeError("Model does not have a predict method")
