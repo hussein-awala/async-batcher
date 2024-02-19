d59fb4f8b491bb57c119557d0bdbc58bb76f6f47
hussein-awala <hussein@awala.fr>
2024-02-19 21:44:56 +0100

    Improve keras example

--- examples/keras/main.py
+++ examples/keras/main.py
@@ -8,8 +8,12 @@ from fastapi import FastAPI
 
 
 class MlBatcher(AsyncBatcher[list[float], list[float]]):
+    def __init__(self, model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+
     async def process_batch(self, batch: list[list[float]]) -> list[float]:
-        batch_result = await asyncio.get_event_loop().run_in_executor(None, model.predict, batch)
+        batch_result = await asyncio.get_event_loop().run_in_executor(None, self.model.predict, batch)
         return batch_result.tolist()
 
 
@@ -17,7 +21,7 @@ app = FastAPI()
 model = tf.keras.models.load_model("../diabetes_tf_model.h5")
 
 
-batcher = MlBatcher(batch_size=200, sleep_time=0.0001)
+batcher = MlBatcher(model=model, batch_size=200, sleep_time=0.0001)
 
 
 @app.on_event("startup")
