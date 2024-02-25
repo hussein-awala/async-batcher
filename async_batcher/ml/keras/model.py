28d87be4cae07ef093c9b23368a5a36b4be881d8
Hussein Awala <hussein@awala.fr>
2024-02-25 21:42:10 +0100

    chore: make the classes b/c with 0.1.0 (#17)

--- async_batcher/ml/keras/model.py
+++ async_batcher/ml/keras/model.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import asyncio
 from typing import TYPE_CHECKING
 
 from async_batcher.batcher import AsyncBatcher
@@ -17,29 +16,35 @@ class KerasAsyncBatcher(AsyncBatcher):
     Args:
         model: The Keras model to use for prediction.
         executor: The executor to use for running the prediction.
-        batch_size (int, optional): The max number of items to process in a batch. Defaults to -1 (no limit).
-        sleep_time (float, optional): The time to sleep between checking if the result is ready in seconds.
-            Defaults to 0.01. Set it to a value close to the expected time to process a batch
-        buffering_time (float, optional): The time to sleep after processing a batch or checking the buffer
-            in seconds. Defaults to 0.001.
-            You can increase this value if you don't need a low latency, but want to reduce the number of
-            processed batches.
+        max_batch_size (int, optional): The max number of items to process in a batch.
+            Defaults to -1 (no limit).
+        max_queue_time (float, optional): The max time for a task to stay in the queue before
+            processing it if the batch is not full and the number of running batches is less
+            than the concurrency. Defaults to 0.01.
+        concurrency (int, optional): The max number of concurrent batches to process.
+            Defaults to 1. If -1, it will process all batches concurrently.
+        executor (Executor, optional): The executor to use to process the batch.
+            If None, it will use the default asyncio executor. Defaults to None.
     """
 
     def __init__(
         self,
         *,
         model: Model,
-        executor: Executor | None = None,
         max_batch_size: int = -1,
-        max_queue_time: float = 0.001,
+        max_queue_time: float = 0.01,
+        concurrency: int = 1,
+        executor: Executor | None = None,
+        **kwargs,
     ):
-        super().__init__(max_batch_size=max_batch_size, max_queue_time=max_queue_time)
-        self.model = model
-        self.executor = executor
-
-    async def process_batch(self, batch):
-        result = await asyncio.get_event_loop().run_in_executor(
-            self.executor, lambda _batch: self.model.predict(_batch, batch_size=len(_batch)), batch
+        super().__init__(
+            max_batch_size=max_batch_size,
+            max_queue_time=max_queue_time,
+            concurrency=concurrency,
+            executor=executor,
+            **kwargs,
         )
-        return result
+        self.model = model
+
+    def process_batch(self, batch):
+        return self.model.predict(batch, batch_size=len(batch))
